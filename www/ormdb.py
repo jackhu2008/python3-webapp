90af06168bb3f91e67cba853d00351df2d085844
jack hu <huwei.jack@foxmail.com>
2016-10-28 16:04:56 +0800

    add orm

---
+++ www/ormdb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import asyncio, logging, aiomysql
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop = loop
+    )
