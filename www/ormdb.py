6611a0c181673d8c977f74623d11910c962b1ae1
jack hu <huwei.jack@foxmail.com>
2016-10-31 21:06:28 +0800

    update

--- www/ormdb.py
+++ www/ormdb.py
@@ -94,6 +94,42 @@ class Model(dict, metaclass=ModelMetaclass):
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r'''Model' object has no arrtribute '%s''' % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self._mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug("using default value for %s: %s" % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        'find object by primary key.'
+        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: $s' % rows)
+
 class Field(object):
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
@@ -113,3 +149,19 @@ def create_args_string(num):
     for n in range(num):
         L.append('?')
     return ', '.join(L)
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
